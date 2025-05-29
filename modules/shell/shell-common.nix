54018f93693ec42bfa7796cda632c19c0cf5df07
Hzbeta <hzbeta@yahoo.com>
2025-05-29 11:38:12 +0800

    feat(shell): update `hms` alias to include --flake option and add `hmsp` alias

--- modules/shell/shell-common.nix
+++ modules/shell/shell-common.nix
@@ -1,7 +1,8 @@
 {
   shellAliases = {
-    hms = "home-manager switch";
-    hmsb = "home-manager switch -b home-manager-backup";
     nfu = "nix flake update --flake ~/.config/home-manager";
+    hms = "home-manager switch --flake ~/.config/home-manager";
+    hmsb = "home-manager switch --flake ~/.config/home-manager -b home-manager-backup";
+    hmsp = "git -C ~/.config/home-manager pull && hms";
   };
 }
